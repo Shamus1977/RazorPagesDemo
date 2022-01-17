45199db8342fb838b764f470a770e54c56693d5f
Shay Johnson <shjohnson413@outlook.com>
2022-01-16 16:09:18 -0800

    Added Category Model and Models folder, Added Data folder and DbContext, added packages Intityframeworkcore, sql server, tools and design. Connected DbContext to sevices using connection string stored in appsettings.json

---
+++ RazorPagesDemo/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesDemo.Models;
+
+namespace RazorPagesDemo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) 
+            : base(options)
+        {
+        }
+        DbSet<Category>? Categories { get; set; }
+    }
+}
