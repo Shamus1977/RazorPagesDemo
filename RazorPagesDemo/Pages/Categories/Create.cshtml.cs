5b964b954ce060ddbf3fc08ce5d03050fea2544b
Shay Johnson <shjohnson413@outlook.com>
2022-02-02 16:32:46 -0600

    Added Create Functioality.

--- RazorPagesDemo/Pages/Categories/Create.cshtml.cs
+++ RazorPagesDemo/Pages/Categories/Create.cshtml.cs
@@ -1,14 +1,29 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesDemo.Data;
 using RazorPagesDemo.Models;
+
 
 namespace RazorPagesDemo.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
+        private readonly AppDbContext? _dbContext;
         public Category? Category { get; set; }
+        public CreateModel(AppDbContext context)
+        {
+            _dbContext = context;
+        }
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            await _dbContext.Categories.AddAsync(Category);
+            await _dbContext.SaveChangesAsync();
+            return RedirectToPage("Index");
         }
     }
 }
