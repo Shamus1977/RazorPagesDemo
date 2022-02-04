bd3a724ce131939ad2966b71a3783a28584b5b94
Shay Johnson <shjohnson413@outlook.com>
2022-02-04 11:29:54 -0600

    Added Delete Functionality for Categories. Fixed bug in the Edit feature for Categories.

---
+++ RazorPagesDemo/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesDemo.Data;
+using RazorPagesDemo.Models;
+
+
+namespace RazorPagesDemo.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly AppDbContext? _dbContext;
+        public Category? Category { get; set; }
+        public DeleteModel(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+        public void OnGet(int id)
+        {
+            Category = _dbContext.Categories.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            Category category = _dbContext.Categories.Find(Category.Id);
+            if(category != null)
+            {
+                _dbContext.Remove(category);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
