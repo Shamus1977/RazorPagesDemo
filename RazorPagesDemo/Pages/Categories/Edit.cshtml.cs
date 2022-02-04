9893b16f7204d000f80ac6da4256a5bf8e88fe26
Shay Johnson <shjohnson413@outlook.com>
2022-02-03 21:56:01 -0600

    Addded Edit functionality to Categories.

---
+++ RazorPagesDemo/Pages/Categories/Edit.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPagesDemo.Data;
+using RazorPagesDemo.Models;
+
+
+namespace RazorPagesDemo.Pages.Categories
+{
+    [BindProperties]
+    public class EditModel : PageModel
+    {
+        private readonly AppDbContext? _dbContext;
+        public Category? Category { get; set; }
+        public EditModel(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+        public void OnGet(int id)
+        {
+            Category = _dbContext.Categories.Find(id);
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(Category.Name == Category.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError(string.Empty, "Category Name and Display Order must not be the same.");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(Category);
+                await _dbContext.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
+        }
+    }
+}
